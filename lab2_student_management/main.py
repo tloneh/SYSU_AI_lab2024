a8bfb1e4493f748334a27ad206e2365032f15aab
hugy <153108732+tloneh@users.noreply.github.com>
2024-04-16 21:13:36 +0800

    Update main.py

--- lab2_student_management/main.py
+++ lab2_student_management/main.py
@@ -1,1 +1,16 @@
+from studata import *
 
+file_name = "student_data.txt"
+out_file = "export_data.txt"
+
+students = StuData(file_name)
+
+students.AddData("hugy", '001', 'M', '20')
+
+students.SortData('name')
+# students.SortData('stu_num')
+# students.SortData('gender')
+# students.SortData('age')
+# print(students.data)
+
+students.ExportFile(out_file)
